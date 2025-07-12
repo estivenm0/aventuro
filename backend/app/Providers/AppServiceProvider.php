4a81f0633f1e07d31fabc9147e73fb7cc3fd0b90
estivenm0 <estivenyeizon@gmail.com>
2025-07-12 14:27:36 -0500

    docs: ✏️ add documentation for api v1

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -30,13 +30,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
-            return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+            return config('app.frontend_url') . "/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });
 
         // Gate::policy(MoonshineUser::class, MoonshineUserPolicy::class);
         // Gate::policy(MoonshineUserRole::class, MoonshineUserRolePolicy::class);
 
         JsonResource::wrap('list');
+
+        Scramble::registerApi('v1', [
+            'api_path' => 'api/v1',
+            'api_path' => 'api'
+        ]);
 
         Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
             $openApi->secure(
